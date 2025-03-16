4927e0657c555754b771ccb52b7f4737da3bb868
Arjun.Krishna <arjunkrishna1282@gmail.com>
2025-03-17 01:00:32 +0530

    comparison added

---
+++ protection/protection_analysis.py
@@ -0,0 +1,10 @@
+from protection_metrics import calculate_protection_metrics
+from protection_visuals import generate_heatmap, generate_histograms
+
+def analyze_protection(original_img, protected_img):
+    """Orchestrate full protection analysis"""
+    return {
+        'metrics': calculate_protection_metrics(original_img, protected_img),
+        'heatmap': generate_heatmap(original_img, protected_img),
+        'histograms': generate_histograms(original_img, protected_img)
+    }
